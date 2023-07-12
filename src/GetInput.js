f7f8b674c89d793a95594d8fc77a164f86978752
CrissRT <smartcristian2002@gmail.com>
2023-07-12 17:44:08 +0300

    Final Stage

--- src/GetInput.js
+++ src/GetInput.js
@@ -4,20 +4,19 @@ import React from "react";
 export default class GetInput extends React.Component {
     render() {
       return (
-        <div id="editor-block" className="d-block text-white">
-            <div id="editor-toolbar" className="d-flex p-1 mx-1 border border-black rounded-top">
-            <i className="bi bi-pencil-fill px-1" />
+        <div id="editor-block" className="container-fluid text-white">
+            <div id="editor-toolbar" className="d-flex p-1 border border-black rounded-top">
+                <i className="bi bi-pencil-fill px-1" />
                     Editor
-                </div>
-            <div id="editor-text" className="px-1">
-                <textarea
-                    id="editor"
-                    value={this.props.input} 
-                    onChange={this.props.handleChange}
-                    className="text-black border border-black rounded-bottom"
-                />           
-            {/* <input value={this.props.input} onChange={this.props.handleChange}/> */}
             </div>
+
+            <textarea
+                id="editor"
+                value={this.props.input} 
+                onChange={this.props.handleChange}
+                className="text-black border border-black rounded-bottom p-2"
+            />           
+            {/* <input value={this.props.input} onChange={this.props.handleChange}/> */}
         </div>
       );
     }
--- src/RenderInput.js
+++ src/RenderInput.js
@@ -4,12 +4,12 @@ import ReactMarkdown from 'react-markdown';
 export default class RenderInput extends React.Component {
     render() {
       return (
-        <div className="d-block text-white" id="preview-block">
+        <div className="container-fluid text-white" id="preview-block">
             <div id="preview-toolbar" className="d-flex p-1 border border-black rounded-top">
                 <i className="bi bi-eye-fill px-1" />
                 Preview
             </div>
-            <div id="preview" className="px-2 text-black border border-black rounded-bottom">
+            <div id="preview" className="p-2 text-black border border-black rounded-bottom">
                 <ReactMarkdown>{this.props.input}</ReactMarkdown>
             </div>
         </div>
