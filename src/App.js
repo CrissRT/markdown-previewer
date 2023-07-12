7014211dcdc1ffd416ad8198a843b0d5bb318aa0
CrissRT <smartcristian2002@gmail.com>
2023-07-12 15:16:44 +0300

    final site

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import RenderInput from './RenderInput';
 import GetInput from './GetInput';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
+
+const firstStart = `# Heading 1
+## Subheading 2
+[Link Text](https://www.example.com)
+\`Inline Code\`
+\`\`\`
+Code Block
+\`\`\`
+- List Item
+> Blockquote
+![Image Alt Text](https://www.example.com/image.jpg)
+**Bolded Text**`;
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: ''
+      inputValue: firstStart
     }
   this.handleChange = this.handleChange.bind(this);
   }
@@ -20,8 +33,8 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        <h1 className="h1 text-center mt-5 mb-5">Markdown Previewer</h1>
-        <div className="d-flex justify-content-around">
+        <h1 className="h1 text-center mb-5">Markdown Previewer</h1>
+        <div className="d-flex justify-content-around" id='markdown-block'>
           <GetInput input={this.state.inputValue} handleChange={this.handleChange}/>
           <RenderInput input={this.state.inputValue}/>
         </div>
