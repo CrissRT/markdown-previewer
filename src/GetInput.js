de87c7c380535d98ca5b88409234003cb3a20b7f
CrissRT <smartcristian2002@gmail.com>
2023-07-12 16:27:31 +0300

    Final stage

--- src/GetInput.js
+++ src/GetInput.js
@@ -5,7 +5,7 @@ export default class GetInput extends React.Component {
     render() {
       return (
         <div id="editor-block" className="d-block text-white">
-            <div id="editor-toolbar" className="d-flex p-1 mx-1 border border-black">
+            <div id="editor-toolbar" className="d-flex p-1 mx-1 border border-black rounded-top">
             <i className="bi bi-pencil-fill px-1" />
                     Editor
                 </div>
@@ -14,7 +14,7 @@ export default class GetInput extends React.Component {
                     id="editor"
                     value={this.props.input} 
                     onChange={this.props.handleChange}
-                    className="text-black border border-black"
+                    className="text-black border border-black rounded-bottom"
                 />           
             {/* <input value={this.props.input} onChange={this.props.handleChange}/> */}
             </div>
--- src/RenderInput.js
+++ src/RenderInput.js
@@ -5,11 +5,11 @@ export default class RenderInput extends React.Component {
     render() {
       return (
         <div className="d-block text-white" id="preview-block">
-            <div id="preview-toolbar" className="d-flex p-1 border border-black">
+            <div id="preview-toolbar" className="d-flex p-1 border border-black rounded-top">
                 <i className="bi bi-eye-fill px-1" />
                 Preview
             </div>
-            <div id="preview" className="px-2 text-black border border-black">
+            <div id="preview" className="px-2 text-black border border-black rounded-bottom">
                 <ReactMarkdown>{this.props.input}</ReactMarkdown>
             </div>
         </div>
